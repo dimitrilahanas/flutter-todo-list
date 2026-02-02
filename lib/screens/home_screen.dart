e1365b2fb7df2aef21fd141349a82fc963339baf
Dimitrios Lahanas <dimitrilahanas@gmail.com>
2026-02-02 14:53:35 +1100

    change(HomeScreen): format document.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,7 +7,9 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("To Do", style: TextStyle(fontWeight: FontWeight.bold))),
+      appBar: AppBar(
+        title: Text("To Do", style: TextStyle(fontWeight: FontWeight.bold)),
+      ),
 
       body: TodoList(),
 
