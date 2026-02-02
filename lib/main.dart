68c316a038d0c868677e303d5157eeaa23822fbf
Dimitrios Lahanas <dimitrilahanas@gmail.com>
2026-02-02 12:42:36 +1100

    add(HomeScreen): added class home screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo_list/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
