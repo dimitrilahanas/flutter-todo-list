c192fa017ed82a6556e7251f139e1292df229e94
Dimitrios Lahanas <dimitrilahanas@gmail.com>
2026-02-02 19:27:32 +1100

    feat(DatabaseService): added add, update, delete, get all.

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_todo_list/model/task.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
@@ -6,15 +7,53 @@ class DatabaseService {
   static const String _dbName = "Todo.db";
 
   static Future<Database> _getDB() async {
-    return openDatabase(join(await getDatabasesPath(), _dbName),
-    onCreate: (db, version) async => 
-    await db.execute('''
+    return openDatabase(
+      join(await getDatabasesPath(), _dbName),
+      onCreate: (db, version) async => await db.execute('''
       CREATE TABLE Tasks (
         id INTEGER PRIMARY KEY,
         name TEXT NOT NULL, 
-        isdone INTEGER NOT NULL
+        isChecked INTEGER NOT NULL
       );'''),
-    version: _version
+      version: _version,
     );
+  }
+
+  static Future<int> addTask(Task task) async {
+    final db = await _getDB();
+    return await db.insert(
+      "Tasks",
+      task.toJson(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  static Future<int> updateTask(Task task) async {
+    final db = await _getDB();
+    return await db.update(
+      "Tasks",
+      task.toJson(),
+      where: 'id = ?',
+      whereArgs: [task.id],
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  static Future<int> deleteTask(Task task) async {
+    final db = await _getDB();
+    return await db.delete(
+      "Tasks",
+      where: 'id = ?',
+      whereArgs: [task.id],
+    );
+  }
+
+  static Future<List<Task>?> getAllTasks() async {
+    final db = await _getDB();
+    final List<Map<String,dynamic>> maps = await db.query("Tasks");
+    if(maps.isNotEmpty){
+      return null;
+    }
+    return List.generate(maps.length, (index) => Task.fromJson(maps[index]));
   }
 }
