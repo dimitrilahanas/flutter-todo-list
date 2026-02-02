b7aa3059ee0e021917cac66d0cccde4bcd027449
Dimitrios Lahanas <dimitrilahanas@gmail.com>
2026-02-02 16:28:01 +1100

    change(HomeScreen): changed to stateful widget for texteditingcontroller.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo_list/widgets/creation_form.dart';
 import 'package:flutter_todo_list/widgets/todo_list.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
+  final nameController = TextEditingController();
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -14,7 +16,14 @@ class HomeScreen extends StatelessWidget {
       body: TodoList(),
 
       floatingActionButton: FloatingActionButton(
-        onPressed: null,
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return CreationForm(controller: );
+            },
+          );
+        },
         child: Icon(Icons.add),
       ),
     );
