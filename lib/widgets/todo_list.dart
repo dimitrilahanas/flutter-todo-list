e424db5bee88a899c0a05cb86e63343810ea032b
Dimitrios Lahanas <dimitrilahanas@gmail.com>
2026-02-02 20:24:31 +1100

    add(TodoList): added Futurebuilder that uses get all tasks.

---
+++ lib/widgets/todo_list.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todo_list/model/task.dart';
+import 'package:flutter_todo_list/services/database_service.dart';
+import 'package:flutter_todo_list/widgets/todo_tile.dart';
+
+class TodoList extends StatelessWidget {
+  final VoidCallback onRefresh;
+  const TodoList({super.key, required this.onRefresh});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 30, right: 30),
+      child: FutureBuilder<List<Task>?>(
+        future: DatabaseService.getAllTasks(),
+        builder: (context, AsyncSnapshot<List<Task>?> snapshot) {
+          final tasks = snapshot.data ?? [];
+          return ListView.builder(
+            itemCount: tasks.length,
+            itemBuilder: (context, index) {
+              final task = tasks[index];
+              return TodoTile(
+                isChecked: task.isChecked,
+                task: task,
+                onDeletePressed: () async {
+                  await DatabaseService.deleteTask(task);
+                  onRefresh();
+                }, 
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
