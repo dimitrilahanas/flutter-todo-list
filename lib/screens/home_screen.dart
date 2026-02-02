473e9e8cf27809f85cbea57584221fa5b22fdc98
Dimitrios Lahanas <dimitrilahanas@gmail.com>
2026-02-02 17:10:51 +1100

    change(HomeScreen): changed to stateful widget.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,29 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:flutter_todo_list/widgets/creation_form.dart';
 import 'package:flutter_todo_list/widgets/todo_list.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   final nameController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("To Do", style: TextStyle(fontWeight: FontWeight.bold)),
+        title: const Text(
+          "To Do",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
       ),
 
-      body: TodoList(),
+      body: const TodoList(),
 
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showDialog(
             context: context,
             builder: (context) {
-              return CreationForm(controller: );
+              return CreationForm(controller: nameController, onPressed: () {});
             },
           );
         },
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
       ),
     );
   }
