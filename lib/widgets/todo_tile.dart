4058c9c3677be44b46243dc367024cec7b03cd5d
Dimitrios Lahanas <dimitrilahanas@gmail.com>
2026-02-02 17:32:36 +1100

    feat(TodoTile): added delete button & checkbox.

--- lib/widgets/todo_tile.dart
+++ lib/widgets/todo_tile.dart
@@ -1,33 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class TodoTile extends StatelessWidget {
   final bool isChecked;
   final String taskName;
-  const TodoTile({super.key, required this.isChecked, required this.taskName});
+  final void Function(bool?)? onCheckboxChanged;
+  final void Function() onDeletePressed;
+
+  const TodoTile({super.key, required this.isChecked, required this.taskName, this.onCheckboxChanged, required this.onDeletePressed});
 
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
 
-    return Container(
-      height: screenHeight * 0.08,
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.onPrimary,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Row(
-        children: [
-          Checkbox(value: isChecked, onChanged: null),
-
-          Text(
-            taskName,
-            style: TextStyle(
-              fontWeight: FontWeight.w400,
-              letterSpacing: 0,
-              fontSize: 18,
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(12),
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: StretchMotion(),
+          children: [
+            SlidableAction(
+              onPressed: (context) => onDeletePressed,
+              icon: Icons.delete,
             ),
+          ],
+        ),
+        child: Container(
+          height: screenHeight * 0.08,
+          color: Theme.of(context).colorScheme.onPrimary,
+
+          child: Row(
+            children: [
+              Checkbox(value: isChecked, onChanged: onCheckboxChanged),
+
+              Text(
+                taskName,
+                style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  fontSize: 18,
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
